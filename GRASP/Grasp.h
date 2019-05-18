de5bfe22185b7327c9faca7ec526f46ee522e156
alu0101028163 <alu0101028163@ull.edu.es>
2019-05-18 22:47:53 +0100

    Implemented and tested isVisited method

--- GRASP/Grasp.h
+++ GRASP/Grasp.h
@@ -16,8 +16,32 @@ namespace Grasp{
     int calculateLabel(int parentNode, std::list<int>& remainingLabels, std::vector<int>& labeling);
     std::vector<int>& calculateCandidateList(int parentNode, std::list<int>& remainingLabels, std::vector<int>& labeling);
     int calculateCardinality(std::list<int>& remainingLabels);
+
+
+    /**
+     * labeling is a vector representing the label value for each node,
+     * knowing that labels lower or equal to zero cannot exists we check
+     * if the label value in the node position is different than zero, which is
+     * the value it was initialized with.
+     * @param  node
+     * @param  labeling 
+     * @return
+     */
     bool isVisited(int node, std::vector<int>& labeling);
+
+
+    /**
+     * It returns a random value from a vector of integers representing labels.
+     * @param  labels
+     * @return
+     */
     int getRandomLabel(std::vector<int>& labels);
+
+    /**
+     * It returns a random value from a list of integers representing labels.
+     * @param  labels
+     * @return
+     */
     int getRandomLabel(std::list<int>& labels);
 
 }
--- GRASP/Grasp_test.cpp
+++ GRASP/Grasp_test.cpp
@@ -10,12 +10,10 @@ TEST_CASE("Get random label from vector works properly"){
   std::vector<int> label(arr, arr + sizeof(arr) / sizeof(arr[0]) );
 
   int label1 = Grasp::getRandomLabel(label);
-  std::cout << label1 << "\n";
   REQUIRE(label1 >= 1);
   REQUIRE(label1 <= 4);
 
   int label2 = Grasp::getRandomLabel(label);
-  std::cout << label2 << "\n";
   REQUIRE(label2 >= 1);
   REQUIRE(label2 <= 4);
 
@@ -25,12 +23,24 @@ TEST_CASE("Get random label from list works properly"){
 
   std::list<int> label({1,2,3,4});
   int label1 = Grasp::getRandomLabel(label);
-  std::cout << label1 << "\n";
   REQUIRE(label1 >= 1);
   REQUIRE(label1 <= 4);
 
   int label2 = Grasp::getRandomLabel(label);
-  std::cout << label2 << "\n";
   REQUIRE(label2 >= 1);
   REQUIRE(label2 <= 4);
+}
+
+
+TEST_CASE("Is visited works properly"){
+
+  static const int arr[] = {1,0,0,3};
+  std::vector<int> label(arr, arr + sizeof(arr) / sizeof(arr[0]) );
+
+  REQUIRE(Grasp::isVisited(0,label) == true);
+  REQUIRE(Grasp::isVisited(1,label) == false);
+  REQUIRE(Grasp::isVisited(2,label) == false);
+  REQUIRE(Grasp::isVisited(3,label) == true);
+
+
 }
