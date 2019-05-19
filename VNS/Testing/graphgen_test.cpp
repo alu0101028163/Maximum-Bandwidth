6019ea69914d92b707215f0beefdb33364764330
Fernando González <alu0101045870@ull.edu.es>
2019-05-19 23:22:20 +0100

    Restructuring and adding functional tests with catch!

---
+++ VNS/Testing/graphgen_test.cpp
@@ -0,0 +1,4 @@
+#include "GraphGenerator.h"
+#include "catch.hpp"
+
+TEST_CASE("2. Do a flip (empty)", "[tests_from_vs:2]") {}
---
+++ VNS/Testing/neighborhood_test.cpp
@@ -0,0 +1,4 @@
+#include "neighborhood.h"
+#include "catch.hpp"
+
+TEST_CASE("3: Do another flip (empty)", "[tests_from_vs:3]") {}
