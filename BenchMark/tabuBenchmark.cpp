48601086f6820d0e59e3ff8fb4414400437b8c49
alu0101028163 <alu0101028163@ull.edu.es>
2019-05-21 11:10:34 +0100

    Added security copies and avoid re-execution of already executed benchmarks

--- BenchMark/tabuBenchmark.cpp
+++ BenchMark/tabuBenchmark.cpp
@@ -22,10 +22,10 @@ int main(int argc, char *argv[]){
 
 
   std::vector<int> label = GraphGen::readLabel(graspLabel);
-  for(int i = 0; i < label.size(); i++)
-     std::cout << label[i] << " ";
-  std::cout << "\n";
-  std::cout << "SIZE: " << label.size() << "\n";
+  // for(int i = 0; i < label.size(); i++)
+  //    std::cout << label[i] << " ";
+  // std::cout << "\n";
+  // std::cout << "SIZE: " << label.size() << "\n";
   std::vector<std::vector<short int > > graph = GraphGen::disperseFileToGraph("../" + instancePath);
 
 
