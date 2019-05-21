0b93d20b7a46c5f42a9c391f64994a3ed227c474
alu0101028163 <alu0101028163@ull.edu.es>
2019-05-21 01:25:12 +0100

    Unfunctional testTabu

--- BenchMark/benchmarkMaster.py
+++ BenchMark/benchmarkMaster.py
@@ -1,14 +1,13 @@
 import os
 import re
+import pandas as pd
 
 paths = []
 paths.append("Literature_Instances/hb/bcspwr01.mtx.rnd")
 paths.append("Literature_Instances/hb/bcspwr02.mtx.rnd")
-paths.append("Literature_Instances/hb/ibm32.mtx.rnd")
 paths.append("Literature_Instances/hb/ash85.mtx.rnd")
+paths.append("Literature_Instances/hb/ibm32.mtx.rnd")
 paths.append("Literature_Instances/hb/will57.mtx.rnd")
-
-# paths["Literature_Instances/Grid_Graphs/mesh9_9.txt"] = 36
 
 
 def benchGrasp():
@@ -17,20 +16,66 @@ def benchGrasp():
     grasp_command += "../AntiBandwidth/AntiBandwidth.cpp ../GraphGenerator/GraphGenerator.cpp ../GRASP/Grasp.cpp"
 
     if (os.system(grasp_command) != 0):
-        print("Failed compiling the GRASP algorithm")
+        print("Error trying to compile GRASP benchmark")
         quit()
 
-    for path in paths:
+    for key in paths:
 
-        instance_path = "../" + path
+        instance_path = key
         instance_name_regex = re.compile("\/(\w+)\.")
-        instance_name = instance_name_regex.search(path)
+        instance_name = instance_name_regex.search(key)
         instance_name = instance_name.group(1)
+        instance_value = paths[key]
 
-        grasp_command = "./Grasp_Benchmark " + instance_path + " " + instance_name
+        grasp_command = "./Grasp_Benchmark " + instance_path + " " + instance_name + " " + str(instance_value)
 
         if (os.system(grasp_command) != 0):
-            print("Error trying to test " + instance_path )
+            print("Error trying to test " + instance_path + " during GRASP" )
+            quit()
+
 
 
 def benchTabu():
+
+    tabu_command = "g++ -g -std=c++11 -o Tabu_Benchmark tabuBenchmark.cpp ../AntiBandwidth/catch.hpp ../GraphGenerator/GraphGenerator.cpp"
+    tabu_command += " ../GRASP/Grasp.cpp ../AntiBandwidth/AntiBandwidth.cpp ../TabuSearch/TabuSearch.cpp"
+
+    if (os.system(tabu_command) != 0):
+        print("Error trying to compile TABU benchmark")
+        quit()
+
+    for filename in os.listdir("./GraspCalculations"):
+        print(filename)
+
+        df = pd.read_csv('./GraspCalculations/'+filename)
+        max_label = df.loc[(df['value'] == df['value'].max())]['labeling'].unique()
+        max_label = max_label[0].split(" ")
+
+        file = open("temp_label.txt","w+")
+        for i in range(len(max_label ) - 2):
+            # print("Element: " + max_label[i + 1])
+            file.write(max_label[i + 1] + "\n")
+        file.close()
+
+        instance_name_regex = re.compile("_(\w+)\.csv")
+        instance_name = instance_name_regex.search(filename)
+        instance_name = instance_name.group(1)
+
+        print(instance_name)
+        instance_path = "null"
+
+        for path in paths:
+            if instance_name in path:
+                instance_path = path
+
+        if(instance_path == "null"):
+            print("Error trying to get the path for the file " + instance_name + " during tabu search")
+
+        tabu_command = "./Tabu_Benchmark " + instance_path + " " + instance_name + " temp_label.txt"
+        print(tabu_command)
+
+        if (os.system(tabu_command) != 0):
+            print("Error trying to test " + instance_name + " during TABU search")
+            quit()
+
+benchTabu()
--- BenchMark/tabuBenchmark.cpp
+++ BenchMark/tabuBenchmark.cpp
@@ -2,9 +2,23 @@
 #include "../GRASP/Grasp.h"
 #include "../GraphGenerator/GraphGenerator.h"
 
-int main(){
+int main(int argc, char *argv[]){
 
-  std::vector<int> label = GraphGen::readLabel("./GraspCalculations/temp_label.txt");
+
+  if (argc != 4){
+    std::cout << "Wrong use: \nFirst argument is the path of the instance you want to test."
+              << "\nSecond argument is the name of the instance you want to test."
+              << "\nThird argument is the file that contains best GRASP label.";
+    exit(1);
+  }
+
+
+  std::string instancePath = std::string(argv[1]);
+  std::string instanceName = std::string(argv[2]);
+  std::string graspLabel = std::string(argv[3]);
+
+
+  std::vector<int> label = GraphGen::readLabel("/home/aalvarez/Documents/UNIVERSITY/DYADA/maxbandwidth/BenchMark/temp_label_bcspwr01.txt");
   for(int i = 0; i < label.size(); i++)
      std::cout << label[i] << " ";
   std::cout << "\n";
